0b9c27c9b865f43edbb9fcebe7fd27d7c01231a7
Steve Roebuck <sgroebuck@gmail.com>
2017-02-10 15:14:15 +0000

    Added more

--- setup.sh
+++ setup.sh
@@ -8,14 +8,35 @@ case "$LVER" in
 
 *Ubuntu*)
 PKMGR="apt-get"
+DISTRO="U"
 ;;
 *Redhat*)
 PKMGR="yum"
+DISTRO="R"
 ;;
 esac
 echo $PKMGR
 
 $PKMGR update -y
 
-$PKMGR install libfuse-dev open-vm-tools open-vm-tools-desktop
+$PKMGR install libfuse-dev open-vm-tools open-vm-tools-desktop git -y
+
+git clone https://github.com/KittyKatt/screenFetch.git
+cd screenFetch
+chmod +x screenfetch-dev
+mv screenfetch-dev /etc/screenfetch
+
+if $DISTRO = "U"
+then
+	$PKMGR install openssh-server sudo -y
+	echo  screenfetch >> /etc/bash.bashrc
+	echo "$USER	ALL=(ALL:ALL) ALL" >> /etc/sudoers
+
+elif $DISTRO ="R"
+	echo screenfetch >> /etc/bashrc
+then
+fi
+
+
+
 
